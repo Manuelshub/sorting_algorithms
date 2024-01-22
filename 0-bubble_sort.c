84077f7f95a081d84a79cec2467151757ff2917c
Emmanuel <kcemmy03@gmail.com>
2024-01-22 07:12:46 +0100

    Removed a variable

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,14 +9,13 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp, swapped;
+	int temp;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
-		swapped = 0;
 		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -25,10 +24,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
-				swapped = 1;
 			}
 		}
-		if (!swapped)
-			break;
 	}
 }
